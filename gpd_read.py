1de9479401333ce6336a0d056723d1e7a80d0bb8
unaG <veraguo11@163.com>
2022-03-07 11:32:57 +0800

    Update gpd_read.py

--- gpd_read.py
+++ gpd_read.py
@@ -1,17 +1,17 @@
-# import geopandas as gpd
-#
-# dwg_1_1_path = r"E:\projects\dwg2csv\resources\1-1.dwg"
-# dwg_C1_1_path = r"E:\projects\dwg2csv\resources\C1-1.dwg"
-#
-# shape= gpd.read_file(dwg_1_1_path)
-# shape.head()
-# shape['Layer'].value_counts()
-# arr=shape['Layer'].unique()
-# print(arr)
-#
-# for i in arr:
-#     export=shape[(shape.Layer == i)]
-#     export.to_file(driver = 'ESRI Shapefile', filename= i+".shp")
+import geopandas as gpd
+
+dwg_1_1_path = r"1-1.dwg"
+dwg_C1_1_path = r"C1-1.dwg"
+
+shape= gpd.read_file(dwg_1_1_path)
+shape.head()
+shape['Layer'].value_counts()
+arr=shape['Layer'].unique()
+print(arr)
+
+for i in arr:
+    export=shape[(shape.Layer == i)]
+    export.to_file(driver = 'ESRI Shapefile', filename= i+".shp")
 
 
 
